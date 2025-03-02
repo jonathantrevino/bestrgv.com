b9509f536e021f29f27171133691d0af21196a84
jonathantrevino <yeahimjt+github@gmail.com>
2025-03-01 22:14:43 -0600

    updating public

---
+++ app/components/Nav.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+      <Image src='/wordmark.svg' alt='bestrgv wordmark logo' />
+    </nav>
+  )
+}
+
+export default Nav
