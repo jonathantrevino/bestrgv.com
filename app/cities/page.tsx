84a92711abe183738ae627d2dc522f59890d43c8
jonathantrevino <yeahimjt+github@gmail.com>
2025-03-03 09:57:16 -0600

    initializing page

---
+++ app/cities/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../components/Nav'
+
+const page = () => {
+  return (
+    <div>
+      <Nav />
+      <main className='px-5 sm:px-10'>hello</main>
+    </div>
+  )
+}
+
+export default page
